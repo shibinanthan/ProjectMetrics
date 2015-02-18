3f05d3ea99ff72e4f4f81ac52ad96047c97d050a
mahesh <shibinanthan.m@gmail.com>
2015-02-18 19:46:03 +0530

    Base version
    This is base version of project metrics

---
+++ ProjectMetricsDataLayer/DataContext/PMDataContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Linq;
+//using System.Data.Linq;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cognizant.Tools.ProjectMetrics.DataLayer
+{
+    public class PMDataContext : IDisposable
+    {
+        DataContext dataContext = null;
+        
+        public PMDataContext()
+        {
+            dataContext = new DataContext(Constants.ConnectionString);
+        }
+
+        public void Dispose()
+        {
+
+        }
+        public void SubmitChanges()
+        {
+            dataContext.SubmitChanges();
+        }
+
+        public Table<TEntity> GetTable<TEntity>() where TEntity : class
+        {
+            return dataContext.GetTable<TEntity>();
+        }
+
+    }
+}
---
+++ ProjectMetricsUILayer/Models/TaskModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cognizant.Tools.ProjectMetrics.ProjectMetricsUILayer.Models
+{
+    public class TaskModel
+    {
+    }
+}
